976c279f16f84e3f5d11cee09c25f60777f4613a
Niklas Lindblad <ryxali@gmail.com>
2024-03-19 19:52:18 +0100

    cleanup

--- Assets/Scripts/ISlideStep.cs
+++ Assets/Scripts/ISlideStep.cs
@@ -7,15 +7,3 @@ public interface ISlideStep
     void Exit();
     bool MoveNext(ref PlayerInput input);
 }
-
-public struct Item
-{
-    public string name;
-    public Sprite icon;
-}
-
-public interface IInventory
-{
-    void Add(Item item);
-    void Remove(Item item);
-}
